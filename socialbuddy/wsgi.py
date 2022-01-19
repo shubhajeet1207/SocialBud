d2f90b68955d81fb3e8de45a2b4398e9c53c0106
shubhajeet <shubha.pradhan12@gmail.com>
2022-01-19 17:33:40 +0530

    model updated

--- socialbuddy/wsgi.py
+++ socialbuddy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for studybud project.
+WSGI config for socialbuddy project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialbuddy.settings')
 
 application = get_wsgi_application()
